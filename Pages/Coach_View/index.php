92fa945c581cd992bb69902a632c732717b06e92
Filipe Marques de Souza <33045754+FilipeMSouza@users.noreply.github.com>
2021-04-13 02:39:31 -0300

    Add files via upload

---
+++ Pages/Coach_View/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Collapsible sidebar using Bootstrap 3</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="Coach_Style.css">
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+
+</head>
+
+<body>
+
+
+
+<div class="wrapper">
+    <!-- Sidebar Holder -->
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3>Coach View</h3>
+            </h3>
+        </div>
+
+        <ul class="list-unstyled components">
+            <li class="active">
+                <a href="#menu">Over Viem</a>
+
+            </li>
+            <li>
+                <a href="#menu">Players</a>
+
+            </li>
+            <li>
+                <a href="#menu">Resgister New Player</a>
+            </li>
+            <li>
+                <a href="#menu">Free Agent</a>
+            </li>
+        </ul>
+
+
+    </nav>
+
+    <!-- Page Content Holder -->
+    <div id="content">
+
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+
+                <div class="navbar-header">
+                    <button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn" style="background-color: #165D8F; color: #">
+                        <i class="glyphicon glyphicon-align-left"></i>
+                        <span>Toggle Sidebar</span>
+                    </button>
+                </div>
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#" style="color: #0B8AD9;">Page</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+    </div>
+</div>
+
+
+
+
+
+<!-- jQuery CDN -->
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<!-- Bootstrap Js CDN -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- jQuery Custom Scroller CDN -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+
+
+        $('#sidebarCollapse').on('click', function() {
+            $('#sidebar, #content').toggleClass('active');
+            $('.collapse.in').toggleClass('in');
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        });
+    });
+</script>
+</body>
+
+</html>
+
+<?php
+include "../.././Templates/footer.php";
+?>
--- index.php
+++ index.php
@@ -16,13 +16,13 @@ include 'Templates/header.php'
         <div class = "container">
             <form>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">User Name</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <label for="exampleInputEmail1" class="form-label" ></label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="User Name" style="color: #012840">
                 </div>
 
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <label for="exampleInputPassword1" class="form-label"></label>
+                    <input type="password" class="form-control" placeholder="Password" style="color: #012840">
                 </div>
             </form>
             <div class="bttn">
