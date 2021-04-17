a55d134ae5bb4618c17dc6ce1b8a85eb48e2f2c7
Filipe Marques de Souza <33045754+FilipeMSouza@users.noreply.github.com>
2021-04-16 22:54:08 -0300

    Add files via upload

--- Pages/Dashboard/Lista_Jogadores.php
+++ Pages/Dashboard/Lista_Jogadores.php
@@ -1,54 +1,141 @@
 <?php
-include '../../Templates/header.php'
+include '../.././Templates/header.php';
 ?>
 
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <link rel="stylesheet" href="./css/List_Jog.css">
-    </head>
-    <body>
-    <div class="Cont-1">
-        <header class ="Nome_do_Time">
-            <a href="./index.php">Green Bay Packers</a>
-        </header>
-        <button class="Btn_LgO" onclick="location.href='../../index.php'">Sair</button>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./css/List_Jog.css">
+
+</head>
+
+<body>
+
+<div class="Cont-1">
+    <header class="Nome_do_Time">
+        <a href="./index.php">Green Bay Packers</a>
+    </header>
+    <button class="Btn_LgO" onclick="location.href='../../index.php'">Sair</button>
+</div>
+
+
+
+<div class="card-body">
+    <div class="filtros">
+        <div class="filtro1">
+            <div class="input-group flex-nowrap">
+                <input type="text" class="form-control" placeholder="Camiseta" aria-label="Username" aria-describedby="addon-wrapping">
+            </div>
+        </div>
+
+        <div class="filtro2">
+            <div class="input-group flex-nowrap">
+                <input type="text" class="form-control" placeholder="Posicao" aria-label="Username" aria-describedby="addon-wrapping">
+            </div>
+        </div>
+
+        <div class="filtro3">
+            <div class="input-group flex-nowrap">
+                <input type="text" class="form-control" placeholder="Equipe" aria-label="Username" aria-describedby="addon-wrapping">
+            </div>
+        </div>
     </div>
 
-    <div class="Cont-2">
-        <table class="jogadores">
+    <div class="table-responsive">
+        <table class="table tablesorter " id="">
+            <thead class="text">
             <tr>
-                <th>Nome</th>
-                <th>Idade</th>
-                <th>Pontuação</th>
-                <th>Altura</th>
-                <th>Peso</th>
-                <th>Equipe</th>
-                <th>Posição</th>
-                <th>Time anterior</th>
+                <th scope="col">Cammiseta</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Idade</th>
+                <th scope="col">Pontuação</th>
+                <th scope="col">Altura</th>
+                <th scope="col">Peso</th>
+                <th scope="col">Equipe</th>
+                <th scope="col">Posição</th>
+                <th class="text-center">Time anterior</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th id="#" scope="row">12</th>
+                <td id="#">Aaron Rodgers</td>
+                <td id="#">37</td>
+                <td id="#">121.5</td>
+                <td id="#">1,88m</td>
+                <td id="#">102Kg</td>
+                <td id="#">Ofenssive</td>
+                <td id="#">Quarterback</td>
+                <td id="#" class="text-center"> California Golden Bears (Faculdade)</td>
+                <td> <button class="btn btn-primary btn-sm">Editar</button></td>
+                <td> <button class="btn btn-primary btn-sm">Delete</button></td>
             </tr>
             <tr>
-                <td>Aaron Rodgers</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <th id="#" scope="row">12</th>
+                <td id="#">Aaron Rodgers</td>
+                <td id="#">37</td>
+                <td id="#">121.5</td>
+                <td id="#">1,88m</td>
+                <td id="#">102Kg</td>
+                <td id="#">Ofenssive</td>
+                <td id="#">Quarterback</td>
+                <td id="#" class="text-center"> California Golden Bears (Faculdade)</td>
+                <td> <button class="btn btn-primary btn-sm">Editar</button></td>
+                <td> <button class="btn btn-primary btn-sm">Delete</button></td>
+
             </tr>
+            <tr>
+                <th id="#" scope="row">12</th>
+                <td id="#">Aaron Rodgers</td>
+                <td id="#">37</td>
+                <td id="#">121.5</td>
+                <td id="#">1,88m</td>
+                <td id="#">102Kg</td>
+                <td id="#">Ofenssive</td>
+                <td id="#">Quarterback</td>
+                <td id="#" class="text-center"> California Golden Bears (Faculdade)</td>
+                <td> <button class="btn btn-primary btn-sm">Editar</button></td>
+                <td> <button class="btn btn-primary btn-sm">Delete</button></td>
+
+            </tr>
+            <tr>
+                <th id="#" scope="row">12</th>
+                <td id="#">Aaron Rodgers</td>
+                <td id="#">37</td>
+                <td id="#">121.5</td>
+                <td id="#">1,88m</td>
+                <td id="#">102Kg</td>
+                <td id="#">Ofenssive</td>
+                <td id="#">Quarterback</td>
+                <td id="#" class="text-center"> California Golden Bears (Faculdade)</td>
+                <td> <button class="btn btn-primary btn-sm">Editar</button></td>
+                <td> <button class="btn btn-primary btn-sm">Delete</button></td>
+
+            </tr>
+            <tr>
+                <th id="#" scope="row">12</th>
+                <td id="#">Aaron Rodgers</td>
+                <td id="#">37</td>
+                <td id="#">121.5</td>
+                <td id="#">1,88m</td>
+                <td id="#">102Kg</td>
+                <td id="#">Ofenssive</td>
+                <td id="#">Quarterback</td>
+                <td id="#" class="text-center"> California Golden Bears (Faculdade)</td>
+                <td> <button class="btn btn-primary btn-sm">Editar</button></td>
+                <td> <button class="btn btn-primary btn-sm">Delete</button></td>
+
+            </tr>
+            </tbody>
         </table>
     </div>
+</div>
 
-    <div class="Cont-3">
-        <table class="tecinicos">
-            <tr>
-                <th>Nome</th>
-                <th>Idade</th>
-                <th>Email</th>
-            </tr>
-        </table>
-    </div>
+</body>
 
-    </body>
 </html>
