0543c8f44004ff007930409332ee2f3bc0fb3075
Victor Hugo <60026761+vhgomes@users.noreply.github.com>
2021-04-21 23:33:26 -0300

    fiz merda
    consertando a merda

--- index.php
+++ index.php
@@ -1,26 +1,60 @@
 <?php
-    include'./templates/header.php';
+include 'database/models.php';
+include_once 'database/database.ini.php';
+
+use ConexaoPHPPostgres\clienteModel as clienteModel;
+use ConexaoPHPPostgres\mecanicoModel as mecanicoModel;
+
+$clienteModel = new clienteModel($pdo);
+$cliente = $clienteModel->all();
+$mecanicoModel = new mecanicoModel($pdo);
+$mecanico = $mecanicoModel->all();
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include('templates/header.php');
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="./assets/style/IND_Style.css">
-    <title>VHF Mecanica</title>
-</head>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+        <th scope="col">Nome</th>
+        <th scope="col">CPF</th>
+        <th scope="col">Telefone</th>
+        </tr>
+    </thead>
+    <?php foreach ($cliente as $cliente):?>
+    <tbody>
+        <tr>
+            <th scope="row"><?php echo $cliente['nomecliente']?></th>
+            <th scope="row"><?php echo $cliente['cpfcliente']?></th>
+            <th scope="row"><?php echo $cliente['telefone']?></th>
+        </tr>
+    </tbody>
+    <?php endforeach;?>
+</table>
 
-<body>
-    <div class="container">
-        <a href="./pages/create/veiculo.php">alo</a>
-    </div>
-</body>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+        <th scope="col">Mecanico</th>
+        <th scope="col">Telefone</th>
+        <th scope="col">CPF do Mecanico</th>
+        </tr>
+    </thead>
+    <?php foreach ($mecanico as $mecanico):?>
+    <tbody>
+        <tr>
+            <th scope="row"><?php echo $mecanico['nome']?></th>
+            <th scope="row"><?php echo $mecanico['celular']?></th>
+            <th scope="row"><?php echo $mecanico['cpf']?></th>
+        </tr>
+    </tbody>
+    <?php endforeach;?>
+</table>
 
-</html>
 
 <?php
-include'./templates/footer.php';
+include('templates/footer.php');
 ?>
